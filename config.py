01dc1e9615533ef7fc8ebc991043ef99d6278915
JoseALermaIII <jose@JoseALerma.com>
2018-11-09 06:19:31 -0600

    Update config.py added detailed description and attributes to docstring

--- config.py
+++ config.py
@@ -1,9 +1,22 @@
-"""Loads database.ini for use in clashcallerbot_database.py"""
+#! python3
+# -*- coding: utf-8 -*-
+"""Loads database.ini.
+
+Module loads database.ini for use in clashcallerbot_database.py,
+clashcallerbot_search.py, and clashcallerbot_reply.py.
+
+Attributes:
+    config (complex): A configparser.ConfigParser() object containing the sections
+        inside database.ini
+    module_dir (str): String with the absolute path of the module's directory.
+    locations (list): List containing all possible paths of database.ini
+"""
+
 import os
 import sys
 import configparser
 
-# Read database.ini file and make database instance
+# Read database.ini file
 config = configparser.ConfigParser()
 module_dir = os.path.dirname(sys.modules[__name__].__file__)
 locations = [os.path.join(module_dir, 'database.ini'), 'database.ini']
