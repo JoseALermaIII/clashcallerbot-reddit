8e98fda255194af11e2d53b6b9c14881e16cafbb
JoseALermaIII <jose@JoseALerma.com>
2018-12-06 02:29:05 -0600

    Update clashcallerbot.sh added spacing, removed reference to Wiki, and updated to new source file names/locations

--- clashcallerbot.sh
+++ clashcallerbot.sh
@@ -1,10 +1,12 @@
 # clashcallerbot.sh
-# Script to check for and start clashcallerbot scripts as background processes
+#
+# Script to check for and start clashcallerbot scripts as background processes.
+#
 # Notes:
 # * Remember to set executable mode with `chmod +x ./clashcallerbot.sh`
 # * Script assumes files are in same directory and is run as crontab in no-root setup
 #   * How often to run as a crontab depends on how long you want the bot to be down/broken
-#   * If you have access to root, check the Wiki for info on setting up systemd instead
+#   * If you have access to root, check the Docs for info on setting up systemd instead
 # * Logfile can be removed if not necessary (remove variable and `>> $logfile`)
 # * `python` can be replaced with relevant python version
 # * The function in `kill` returns all script PIDs, so it must be restarted.
@@ -13,28 +15,28 @@
 
 logfile="./clashcallerbotlog.txt"
 
-case "$(ps -ef | grep '[p]ython clashcallerbot_reply.py' | wc -l)" in
+case "$(ps -ef | grep '[p]ython reply.py' | wc -l)" in
 
-0) echo "Restarting clashcallerbot_reply.py: $(date)" >> $logfile
-   nohup python clashcallerbot_reply.py > /dev/null 2>&1 &
+0) echo "Restarting reply.py: $(date)" >> $logfile
+   nohup python ./clashcallerbotreddit/reply.py > /dev/null 2>&1 &
    ;;
 1) # Normal
    ;;
-*) echo "Remove duplicate clashcallerbot_reply.py: $(date)" >> $logfile
-   kill $(ps aux | grep '[p]ython clashcallerbot_reply.py' | awk '{print $2}')
-   nohup python clashcallerbot_reply.py > /dev/null 2>&1 &
+*) echo "Remove duplicate reply.py: $(date)" >> $logfile
+   kill $(ps aux | grep '[p]ython reply.py' | awk '{print $2}')
+   nohup python ./clashcallerbotreddit/reply.py > /dev/null 2>&1 &
    ;;
 esac
 
-case "$(ps -ef | grep '[p]ython clashcallerbot_search.py' | wc -l)" in
+case "$(ps -ef | grep '[p]ython search.py' | wc -l)" in
 
-0) echo "Restarting clashcallerbot_search.py: $(date)" >> $logfile
-   nohup python clashcallerbot_search.py > /dev/null 2>&1 &
+0) echo "Restarting search.py: $(date)" >> $logfile
+   nohup python ./clashcallerbotreddit/search.py > /dev/null 2>&1 &
    ;;
 1) # Normal
    ;;
-*) echo "Remove duplicate clashcallerbot_search.py: $(date)" >> $logfile
-   kill $(ps aux | grep '[p]ython clashcallerbot_search.py' | awk '{print $2}')
-   nohup python clashcallerbot_search.py > /dev/null 2>&1 &
+*) echo "Remove duplicate search.py: $(date)" >> $logfile
+   kill $(ps aux | grep '[p]ython search.py' | awk '{print $2}')
+   nohup python search.py > /dev/null 2>&1 &
    ;;
 esac
