c974e187973bb068892115f1a5bd7eeae9683938
JoseALermaIII <jose@JoseALerma.com>
2018-12-09 23:49:31 -0600

    Update __init__.py and manual.pdf bump to v2.4.1

--- clashcallerbotreddit/__init__.py
+++ clashcallerbotreddit/__init__.py
@@ -23,7 +23,7 @@ import os
 import sys
 import configparser
 
-__version__ = '2.4.0'
+__version__ = '2.4.1'
 __all__ = ['__version__', 'config', 'LOGGING']
 
 # Loads database.ini for use in database.py, search.py, and reply.py.
