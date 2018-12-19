cf2357cc4806c2aa0550c3f816d7dcf063888aaf
JoseALermaIII <jose@JoseALerma.com>
2018-12-18 22:47:33 -0600

    Update restart.sh specified python3

--- restart.sh
+++ restart.sh
@@ -17,12 +17,12 @@ source ./env/bin/activate  # set virtual environment
 logfile="./logs/clashcallerbotlog.txt"
 
 echo "Restarting reply.py: $(date)" >> $logfile
-kill $(ps aux | grep '[p]ython -m clashcallerbotreddit.reply' | awk '{print $2}')
+kill $(ps aux | grep '[p]ython3 -m clashcallerbotreddit.reply' | awk '{print $2}')
 sleep 3
-nohup python -m clashcallerbotreddit.reply > /dev/null 2>&1 &
+nohup python3 -m clashcallerbotreddit.reply > /dev/null 2>&1 &
 
 
 echo "Restarting search.py: $(date)" >> $logfile
-kill $(ps aux | grep '[p]ython -m clashcallerbotreddit.search' | awk '{print $2}')
+kill $(ps aux | grep '[p]ython3 -m clashcallerbotreddit.search' | awk '{print $2}')
 sleep 3
-nohup python -m clashcallerbotreddit.search > /dev/null 2>&1 &
+nohup python3 -m clashcallerbotreddit.search > /dev/null 2>&1 &
