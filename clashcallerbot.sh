d548cf0b137b3d4ac258729eb4d77871c190a89f
JoseALermaIII <jose@JoseALerma.com>
2019-01-15 04:26:42 -0600

    Update clashcallerbot.sh changed virtual environment to python3 variable and logfile to single quotes

--- clashcallerbot.sh
+++ clashcallerbot.sh
@@ -13,7 +13,7 @@
 #   * If you have access to ``pidof``, you can avoid killing all script instances
 #
 
-source ./env/bin/activate  # set virtual environment
+python3='./env/bin/python3'
 
 # Make logs directory
 if [ ! -d logs ]; then
@@ -22,30 +22,30 @@ if [ ! -d logs ]; then
     echo '*****'
 fi
 
-logfile="./logs/clashcallerbotlog.txt"
+logfile='./logs/clashcallerbotlog.txt'
 
 case "$(ps -ef | grep '[p]ython3 -m clashcallerbotreddit.reply' | wc -l)" in
 
 0) echo "Starting reply.py: $(date)" >> $logfile
-   nohup python3 -m clashcallerbotreddit.reply > /dev/null 2>&1 &
+   nohup $python3 -m clashcallerbotreddit.reply > /dev/null 2>&1 &
    ;;
 1) # Normal
    ;;
 *) echo "Removing duplicate reply.py: $(date)" >> $logfile
    kill $(ps aux | grep '[p]ython3 -m clashcallerbotreddit.reply' | awk '{print $2}')
-   nohup python3 -m clashcallerbotreddit.reply > /dev/null 2>&1 &
+   nohup $python3 -m clashcallerbotreddit.reply > /dev/null 2>&1 &
    ;;
 esac
 
 case "$(ps -ef | grep '[p]ython3 -m clashcallerbotreddit.search' | wc -l)" in
 
 0) echo "Starting search.py: $(date)" >> $logfile
-   nohup python3 -m clashcallerbotreddit.search > /dev/null 2>&1 &
+   nohup $python3 -m clashcallerbotreddit.search > /dev/null 2>&1 &
    ;;
 1) # Normal
    ;;
 *) echo "Removing duplicate search.py: $(date)" >> $logfile
    kill $(ps aux | grep '[p]ython3 -m clashcallerbotreddit.search' | awk '{print $2}')
-   nohup python3 -m clashcallerbotreddit.search > /dev/null 2>&1 &
+   nohup $python3 -m clashcallerbotreddit.search > /dev/null 2>&1 &
    ;;
 esac
