077f822f2e729276fb31246e8b0d4e895d3cf891
JoseALermaIII <jose@JoseALerma.com>
2019-01-15 04:30:04 -0600

    Update restart.sh changed virtual environment to python3 variable and logfile to single quotes

--- restart.sh
+++ restart.sh
@@ -12,17 +12,17 @@
 #   * If you have access to ``pidof``, you can avoid killing all script instances
 #
 
-source ./env/bin/activate  # set virtual environment
+python3='./env/bin/python3'
 
-logfile="./logs/clashcallerbotlog.txt"
+logfile='./logs/clashcallerbotlog.txt'
 
 echo "Restarting reply.py: $(date)" >> $logfile
 kill $(ps aux | grep '[p]ython3 -m clashcallerbotreddit.reply' | awk '{print $2}')
 sleep 3
-nohup python3 -m clashcallerbotreddit.reply > /dev/null 2>&1 &
+nohup $python3 -m clashcallerbotreddit.reply > /dev/null 2>&1 &
 
 
 echo "Restarting search.py: $(date)" >> $logfile
 kill $(ps aux | grep '[p]ython3 -m clashcallerbotreddit.search' | awk '{print $2}')
 sleep 3
-nohup python3 -m clashcallerbotreddit.search > /dev/null 2>&1 &
+nohup $python3 -m clashcallerbotreddit.search > /dev/null 2>&1 &
