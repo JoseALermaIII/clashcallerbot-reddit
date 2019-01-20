4782040c02e972bcc597768c147d803428f3ab0e
JoseALermaIII <jose@JoseALerma.com>
2019-01-20 00:05:08 -0600

    Update reply.py added AddMe! command process to check_messages()

--- clashcallerbotreddit/reply.py
+++ clashcallerbotreddit/reply.py
@@ -18,11 +18,12 @@ import praw
 import praw.exceptions
 
 import logging.config
+import re
 import datetime
 import time
 
 from clashcallerbotreddit.database import ClashCallerDatabase
-from clashcallerbotreddit.search import is_recent
+from clashcallerbotreddit.search import is_recent, message_re
 from clashcallerbotreddit import LOGGING, config
 
 # Logger
@@ -41,6 +42,20 @@ db = ClashCallerDatabase(config, root_user=False)
 # Time constants
 start_time = datetime.datetime.now(datetime.timezone.utc)
 archive_time = start_time - datetime.timedelta(weeks=12)  # 6 months archival time
+
+# Regular expressions
+addme_re = re.compile(r'''
+                        \[              # opening square bracket (required)
+                        (?P<link_re>
+                        (\S)+)          # any non-whitespace characters (required)
+                        \]              # closing square bracket (required)
+                        (?P<exp_re> 
+                        (\d){4}-        # four digit number followed by hyphen (required)
+                        (\d){1,2}-      # one or two digit number followed by hyphen (required)
+                        (\d){1,2}\s     # one or two digit number followed by space (required)
+                        ((\d){2}:){2}   # two, two digit numbers followed by a colon (required)
+                        (\d){2})        # two digit number followed (required)
+                        ''', re.VERBOSE)
 
 
 def main():
@@ -90,7 +105,37 @@ def check_messages()-> None:
             # Process add command
             elif message.subject == 'AddMe!':
                 logger.info(f'Inbox add: {message.id}.')
-                pass
+                # Get URL and expiration time from message body
+                match = addme_re.search(message.body)
+                if not match:
+                    logger.debug(f'Inbox skip add (bad message format): {message.id}.')
+                    message.delete()
+                    continue
+                link_re = match.group('link_re')
+                exp_re = match.group('exp_re')
+                exp_re += '+0000'  # add UTC offset
+                exp_datetime = datetime.datetime.strptime(exp_re, '%Y-%m-%d %H:%M:%S%z')
+                now = datetime.datetime.now(datetime.timezone.utc)
+                logger.info(f'Comment link: {link_re}.')
+                logger.info(f'Expiration datetime: {exp_datetime}.')
+                if now > exp_datetime:
+                    logger.debug(f'Inbox skip add (expired message): {message.id}')
+                    message.delete()
+                    continue
+                # Get call message from message body
+                match = message_re.search(message.body)
+                if not match:
+                    logger.debug(f'Inbox skip add (bad call message format): {message.id}.')
+                    message.delete()
+                    continue
+                call_message = match.group(0)
+                # Add to database
+                logger.info(f'Inbox add save to db: {message.id}.')
+                db.open_connections()
+                db.save_message(link_re, call_message, exp_datetime, message.author.name)
+                db.close_connections()
+                # Delete message
+                message.delete()
             # Process delete command
             elif message.subject == 'Delete!':
                 logger.info(f'Inbox delete: {message.id}.')
