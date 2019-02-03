73f6a66877cb0fca6165cb3c9818b537b69f4dc7
JoseALermaIII <jose@JoseALerma.com>
2019-02-03 03:15:08 -0600

    Update database.py added remove semicolons in message in save_message()

--- clashcallerbotreddit/database.py
+++ clashcallerbotreddit/database.py
@@ -413,6 +413,8 @@ class ClashCallerDatabase(object):
         exp_mysql = self.convert_datetime(exp)
         if "'" in msg:
             msg = msg.replace("'", "")  # Remove apostrophes in message
+        if ";" in msg:
+            msg = msg.replace(";", "")  # Remove semicolons in message
         try:
             self.lock_write(self._message_table)
             add_row = f'INSERT INTO {self._message_table} (permalink, message, new_date, username) ' \
