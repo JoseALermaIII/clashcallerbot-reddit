8dc48ab87fa45a38a698f465ec61981612092a9e
JoseALermaIII <jose@JoseALerma.com>
2019-02-06 02:14:56 -0600

    Update __init__.py and clashcallerbot-reddit.pdf bump to v2.6.7

--- clashcallerbotreddit/__init__.py
+++ clashcallerbotreddit/__init__.py
@@ -23,7 +23,7 @@ import os
 import sys
 import configparser
 
-__version__ = '2.6.6'
+__version__ = '2.6.7'
 __all__ = ['__version__', 'config', 'LOGGING']
 
 # Loads database.ini for use in database.py, search.py, and reply.py.
